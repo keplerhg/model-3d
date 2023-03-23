204a8b9cf904319972bf71d0c1593e98b01a5be3
Kepler Huaynate <52154074+keplerhg@users.noreply.github.com>
2023-03-23 09:08:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
-const directories = document.querySelectorAll('.directory');
-const modal = document.querySelector('.modal');
-const modalContent = document.querySelector('.modal-content');
-const fileContent = document.querySelector('#file-content');
+const directoryLinks = document.querySelectorAll('.directory-link');
 
-directories.forEach(directory => {
-  directory.addEventListener('click', event => {
+directoryLinks.forEach(link => {
+  link.addEventListener('click', event => {
     event.preventDefault();
-    const fileName = event.target.innerText;
-    modal.style.display = 'flex';
-    fileContent.value = `Contenido del archivo ${fileName}`;
+    const path = event.target.dataset.path;
+    window.location.href = path;
   });
-});
-
-modal.addEventListener('click', event => {
-  if (event.target === modal) {
-    modal.style.display = 'none';
-  }
-});
-
-modalContent.addEventListener('submit', event => {
-  event.preventDefault();
-  console.log(fileContent.value);
-  modal.style.display = 'none';
 });
